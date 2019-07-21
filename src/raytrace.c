5c580412b0bf6a2653eecc2c284c55de7e2076cf
Björn Lindqvist <bjourne@gmail.com>
2019-07-22 01:05:05 +0200

    fixes to make the project build without warnings using msvc

--- src/raytrace.c
+++ src/raytrace.c
@@ -4,7 +4,6 @@
 // on examples from www.scratchapixel.com
 #include <assert.h>
 #include <float.h>
-#include <libgen.h>
 #include <limits.h>
 #include <math.h>
 #include <stdbool.h>
@@ -15,11 +14,12 @@
 
 #include "datatypes/common.h"
 #include "linalg/linalg.h"
+#include "paths/paths.h"
 
 #include "common.h"
 #include "triangle_mesh.h"
 
-bool
+static bool
 v3_array_to_ppm(vec3 *arr, const char *filename,
                   int width, int height) {
     FILE *f = fopen(filename, "wb");
@@ -30,9 +30,9 @@ v3_array_to_ppm(vec3 *arr, const char *filename,
         return false;
     }
     for (int i = 0; i < width * height; i++) {
-        int r = CLAMP(255.0f * arr[i].x, 0, 255);
-        int g = CLAMP(255.0f * arr[i].y, 0, 255);
-        int b = CLAMP(255.0f * arr[i].z, 0, 255);
+        int r = (int)CLAMP(255.0f * arr[i].x, 0, 255);
+        int g = (int)CLAMP(255.0f * arr[i].y, 0, 255);
+        int b = (int)CLAMP(255.0f * arr[i].z, 0, 255);
         if (fprintf(f, "%c%c%c", r, g, b) <= 0) {
             return false;
         }
@@ -54,7 +54,7 @@ typedef struct _raytrace_settings {
     vec3 bg_col;
 } raytrace_settings;
 
-raytrace_settings *
+static raytrace_settings *
 rt_from_args(int argc, char *argv[]) {
     if (argc != 9) {
         return NULL;
@@ -70,46 +70,46 @@ rt_from_args(int argc, char *argv[]) {
         malloc(sizeof(raytrace_settings));
 
     // Scaling and translation
-    me->scale = atof(argv[5]);
-    me->translate.x = atof(argv[6]);
-    me->translate.y = atof(argv[7]);
-    me->translate.z = atof(argv[8]);
+    me->scale = (float)atof(argv[5]);
+    me->translate.x = (float)atof(argv[6]);
+    me->translate.y = (float)atof(argv[7]);
+    me->translate.z = (float)atof(argv[8]);
 
     me->width = width;
     me->height = height;
     me->mesh_file = strdup(argv[1]);
     me->image_file = strdup(argv[2]);
-    me->fov = 50.0393;
-    me->bg_col.x = 0.1;
-    me->bg_col.y = 0.1;
-    me->bg_col.z = 0.2;
+    me->fov = 50.0393f;
+    me->bg_col.x = 0.1f;
+    me->bg_col.y = 0.1f;
+    me->bg_col.z = 0.2f;
     me->position = (vec3){0, 0, -20.0};
     mat4 tmp = {
         {
-            {0.707107, -0.331295, 0.624695, 0},
-            {0, 0.883452, 0.468521, 0},
-            {-0.707107, -0.331295, 0.624695, 0},
-            {-1.63871, -5.747777, -40.400412, 1}
+            {0.707107f, -0.331295f, 0.624695f, 0.0f},
+            {0.0f, 0.883452f, 0.468521f, 0.0f},
+            {-0.707107f, -0.331295f, 0.624695f, 0.0f},
+            {-1.63871f, -5.747777f, -40.400412f, 1.0f}
         }
     };
     me->view = m4_inverse(tmp);
     return me;
 }
 
-void
+static void
 rt_free(raytrace_settings *me) {
     free(me->mesh_file);
     free(me->image_file);
     free(me);
 }
 
-vec3
+static vec3
 rt_ray_direction(raytrace_settings *rt, int x, int y,
                  float aspect_ratio, float scale) {
 
-    float ray_x = (2 * (x + 0.5) / (float)rt->width - 1)
+    float ray_x = (2 * (x + 0.5f) / (float)rt->width - 1)
         * aspect_ratio * scale;
-    float ray_y = (1 - 2 * (y + 0.5) / (float)rt->height)
+    float ray_y = (1 - 2 * (y + 0.5f) / (float)rt->height)
         * scale;
     vec3 dir = {ray_x, ray_y, -1};
     dir = m4_mul_v3d(rt->view, dir);
@@ -125,10 +125,10 @@ shade_intersection(vec3 orig, vec3 dir, ray_intersection *ri,
     vec2 coords;
     tm_get_surface_props(tm, ri, &normal, &coords);
     float n_dot_view = MAX(0.0f, v3_dot(normal, v3_neg(dir)));
-    int m = 10;
-    float checker = (fmod(coords.x * m, 1.0) > 0.5) ^
-        (fmod(coords.y * m, 1.0) < 0.5);
-    float c = 0.3 * (1 - checker) + 0.7 * checker;
+    float m = 10.0f;
+    float checker = (float)((fmod(coords.x * m, 1.0f) > 0.5f) ^
+                            (fmod(coords.y * m, 1.0f) < 0.5f));
+    float c = 0.3f * (1 - checker) + 0.7f * checker;
     return v3_from_scalar(c * n_dot_view);
     #else
     return (vec3){1.0, 1.0, 1.0};
@@ -137,7 +137,7 @@ shade_intersection(vec3 orig, vec3 dir, ray_intersection *ri,
 
 static size_t hits = 0;
 
-vec3
+static vec3
 cast_ray(vec3 orig, vec3 dir, vec3 bg_col, triangle_mesh *tm) {
     ray_intersection ri;
     if (tm_intersect(tm, orig, dir, &ri)) {
@@ -147,13 +147,13 @@ cast_ray(vec3 orig, vec3 dir, vec3 bg_col, triangle_mesh *tm) {
     return bg_col;
 }
 
-void
+static void
 render(raytrace_settings *rt, triangle_mesh *tm, vec3 *fbuf) {
     size_t start = nano_count();
     int w = rt->width;
     int h = rt->height;
     vec3 orig = m4_mul_v3p(rt->view, rt->position);
-    float scale = tan(to_rad(rt->fov * 0.5));
+    float scale = tanf(to_rad(rt->fov * 0.5f));
     float aspect_ratio = (float)w / (float)h;
     for (int y = 0; y < h; y++) {
         for (int x = 0; x < w; x++) {
@@ -164,15 +164,16 @@ render(raytrace_settings *rt, triangle_mesh *tm, vec3 *fbuf) {
     }
     size_t end = nano_count();
     double secs = (double)(end - start) / 1000 / 1000 / 1000;
-    printf("%s %s %d %d %.5f %lu\n",
+    printf("%s %s %s %d %d %.5f %zd\n",
            ISECT_NAME,
-           basename(rt->mesh_file),
+           rt->mesh_file,
+           paths_basename(rt->mesh_file),
            rt->width,
            rt->height,
            secs, hits);
 }
 
-void
+static void
 usage() {
     printf("usage: raytrace mesh-file image "
            "width[0-2048] height[0-2048] scale tx ty tz\n");
--- wscript
+++ wscript
@@ -17,16 +17,27 @@ def configure(ctx):
     ctx.load('compiler_c')
     ctx.define('ISECT_METHOD', ctx.options.isect, quote = False)
     ctx.define('SHADING_STYLE', ctx.options.shading, quote = False)
-    ctx.check(lib = 'm')
+    #ctx.check(lib = 'm')
     ctx.check(lib = 'datatypes')
     ctx.check(lib = 'linalg')
     ctx.check(lib = 'isect')
     ctx.check(lib = 'file3d')
-    base_flags = ['-Wall', '-Werror', '-fPIC', '-std=gnu11']
-    speed_flags = ['-O3',
-                   '-fomit-frame-pointer',
-                   '-march=native',
-                   '-mtune=native']
+    ctx.check(lib = 'paths')
+    if ctx.env.CC_NAME == 'msvc':
+        base_flags = [
+            '/WX', '/W3', '/O2', '/EHsc',
+            # Without these flags, msvc generates a billion bullshit
+            # warnings.
+            '/D_CRT_SECURE_NO_WARNINGS',
+            '/D_CRT_NONSTDC_NO_DEPRECATE'
+        ]
+        speed_flags = []
+    else:
+        base_flags = ['-Wall', '-Werror', '-fPIC', '-std=gnu11']
+        speed_flags = ['-O3',
+                       '-fomit-frame-pointer',
+                       '-march=native',
+                       '-mtune=native']
     ctx.env.append_unique('CFLAGS', base_flags + speed_flags)
 
 def build(ctx):
@@ -36,5 +47,6 @@ def build(ctx):
         'FILE3D',
         'ISECT',
         'LINALG',
+        'PATHS',
         'M',
     ])
